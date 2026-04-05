c39a02c39dae4bc7093996e2726ddf249c12a9fc
André Göpel <22834588+derandre84@users.noreply.github.com>
2026-04-05 20:36:14 +0400

    change pages to radzen

--- AndreGoepel.MembersArea/AndreGoepel.MembersArea/Components/Account/CookieLoginMiddleware.cs
+++ AndreGoepel.MembersArea/AndreGoepel.MembersArea/Components/Account/CookieLoginMiddleware.cs
@@ -11,14 +11,48 @@ public class LoginInfo
     public bool RememberMe { get; set; }
 }
 
+public class TwoFactorLoginInfo
+{
+    public required string Code { get; set; }
+    public bool RememberMe { get; set; }
+    public bool RememberMachine { get; set; }
+    public string? ReturnUrl { get; set; }
+}
+
 public class CookieLoginMiddleware(RequestDelegate next)
 {
     public static IDictionary<Guid, LoginInfo> Logins { get; private set; } =
         new ConcurrentDictionary<Guid, LoginInfo>();
 
+    public static IDictionary<Guid, TwoFactorLoginInfo> TwoFactorLogins { get; private set; } =
+        new ConcurrentDictionary<Guid, TwoFactorLoginInfo>();
+
     public async Task Invoke(HttpContext context, SignInManager<User> signinManager)
     {
-        if (context.Request.Path == "/login" && context.Request.Query.ContainsKey("key"))
+        if (context.Request.Path == "/login2fa" && context.Request.Query.ContainsKey("key"))
+        {
+            var key = Guid.Parse(context.Request.Query["key"]!);
+            var info = TwoFactorLogins[key];
+            TwoFactorLogins.Remove(key);
+
+            var code = info.Code.Replace(" ", string.Empty).Replace("-", string.Empty);
+            var result = await signinManager.TwoFactorAuthenticatorSignInAsync(code, info.RememberMe, info.RememberMachine);
+
+            if (result.Succeeded)
+            {
+                context.Response.Redirect(info.ReturnUrl ?? "/");
+            }
+            else if (result.IsLockedOut)
+            {
+                context.Response.Redirect("/Account/Lockout");
+            }
+            else
+            {
+                context.Response.Redirect("/Account/LoginWith2fa?error=invalid");
+            }
+            return;
+        }
+        else if (context.Request.Path == "/login" && context.Request.Query.ContainsKey("key"))
         {
             var key = Guid.Parse(context.Request.Query["key"]!);
             var info = Logins[key];
